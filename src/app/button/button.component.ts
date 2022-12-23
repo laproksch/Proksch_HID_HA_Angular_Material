6870227ad69eca3aa3a2f0c123fd3ee1e6a9fb68
Lukas Proksch <2120859001@fh-burgenland.at>
2022-12-23 15:57:10 +0100

    UX Feedback 1 * consitent page title * refactoring of add-sensor component * sensor name instead of id in table

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -7,6 +7,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class ButtonComponent implements OnInit {
   @Input() text!: string;
+  @Input() icon!: string;
   @Input() color!: string;
   @Output() btnClick = new EventEmitter();
 
